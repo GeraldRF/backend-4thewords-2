de50934f7dfdff7ae55d0588504ffe23da84fa7b
Gerald Ramírez <geral.rafo@gmail.com>
2022-05-31 02:35:05 -0600

    Se creo la migracion de hitoricalEvents table

--- database/migrations/2022_05_31_082532_create_historical_event.php
+++ database/migrations/2022_05_31_082532_create_historical_event.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('historical_event', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('img_url');
+            $table->text('name', 100);
+            $table->text('description', 150);
+            $table->dateTime('date');
+            $table->int('popularity');
+            $table->text('country', 60);
+            $table->text('coordinates');
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('historical_event');
     }
 };
