9dd914bcbb4f2e1f75e55a057d1379582d3f7541
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-01 17:02:19 -0600

    Se crearon las rutas del api para el crud historical event y para los files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\historicalEventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Api for historical events';
 });
+
+Route::get('historicalEvents/get-upload/{url}', [historicalEventController::class,'getUpload']);
