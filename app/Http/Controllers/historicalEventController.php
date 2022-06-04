2194b683b98f58f8f2ee5655f7afd1febee4733e
Gerald Ramírez <geral.rafo@gmail.com>
2022-06-04 05:09:44 -0600

    se atualizo la direccion del api y se agrego request general a store y update se removieron unured imports

--- app/Http/Controllers/historicalEventController.php
+++ app/Http/Controllers/historicalEventController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreHistoricalEventRequest;
-use App\Http\Requests\UpdateHistoricalEventRequest;
+use Illuminate\Http\Request;
 use App\Http\Resources\historicalEventResource;
 use App\Models\historicalEvent;
 use Illuminate\Http\Response;
@@ -24,12 +23,20 @@ class historicalEventController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreHistoricalEventRequest  $request
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreHistoricalEventRequest $request)
+    public function store(Request $request)
     {
-        $validated = $request->validate();
+        $validated = $request->validate([
+            'img_url' => 'required',
+            'name' => 'required|max:100',
+            'description' => 'required|max:150',
+            'date' => 'required',
+            'popularity' => 'required',
+            'country' => 'required|max:60',
+            'coordinates' => 'required',
+        ]);
         $historicalEvent = historicalEvent::create($validated);
         return new historicalEventResource($historicalEvent);
     }
@@ -49,12 +56,20 @@ class historicalEventController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateHistoricalEventRequest  $request
+     * 
      *      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateHistoricalEventRequest $request, $id)
+    public function update(Request $request, $id)
     {
-        $validated = $request->validate();
+        $validated = $request->validate([
+            'img_url' => 'required',
+            'name' => 'required|max:100',
+            'description' => 'required|max:150',
+            'date' => 'required',
+            'popularity' => 'required',
+            'country' => 'required|max:60',
+            'coordinates' => 'required',
+        ]);
         $historicalEvent = historicalEvent::find($id);
         $historicalEvent->update($validated);
         return new historicalEventResource($historicalEvent);
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\historicalEventController;
+use App\Http\Controllers\fileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('historicalEvents', historicalEventController::class);
-Route::post('/historicalEvents/upload', [historicalEventController::class, 'upload']);
+Route::resource('historical-events', historicalEventController::class);
+Route::post('/historical-events/upload', [fileController::class, 'upload']);
